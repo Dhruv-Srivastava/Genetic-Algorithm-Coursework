97ce4fbfe9cdf99b0dbdaad1c4d0bfd6ee9f7e51
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-04-17 00:02:45 +0530

    Add static assets

--- 01-Maximize Function/utils/generate-inital-population.js
+++
@@ -1,9 +0,0 @@
-import { generateChromosome } from "./generate-chromosome.js";
-
-export function generateInitialPopulation(size, chromosomeLength) {
-  const population = [];
-  for (let i = 0; i < size; i++)
-    population.push(generateChromosome(chromosomeLength));
-
-  return population;
-}
